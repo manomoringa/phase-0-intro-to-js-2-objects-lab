d13a30ecb4047f1dd67466be99c5b4e0d6d214e8
abdirahmanissa <manoissa992@gmail.com>
2024-07-18 19:55:39 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name: "mano",
+    streetAddress: "30200 Kitale Nairobi",
+  };
+  
+  const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  };
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  
+  const deleteFromEmployeeByKey = (employee, key) => {
+    const updatedEmployeeDetails = {
+      ...employee,
+    };
+    delete updatedEmployeeDetails[key];
+    return updatedEmployeeDetails;
+  };
+  
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+  };
